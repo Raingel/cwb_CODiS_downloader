2af8bf125fbfab13e4e1e85b866c57b0401a99ab
Raingel <allenstorm2005@gmail.com>
2019-09-05 09:51:22 +0800

    add comment

--- day_weather_grab_class.php
+++ day_weather_grab_class.php
@@ -6,8 +6,8 @@ class grab_weather{
 		$end_time=strtotime($end_to);
 		while($start_time<=$end_time){
 			$buffer=file_get_contents('https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station='.$station_number.'&stname=%25E9%259E%258D%25E9%2583%25A8&datepicker='.date('Y-m-d',$start_time));
-			$buffer=mb_convert_encoding($buffer,'big5');
-			preg_match_all('/<tr class="second_tr">.*<\/tbody>/s',$buffer,$matches);
+			$buffer=mb_convert_encoding($buffer,'big5');  //Excel read csv in big5 by default
+			preg_match_all('/<tr class="second_tr">.*<\/tbody>/s',$buffer,$matches);  //parsing the table
 			preg_match_all('/(?:<tr class="second_tr">|<tr>).*?<\/tr>/s',$matches[0][0],$matches1);
 			//print_r($matches1);
 			//echo $buffer;
@@ -15,18 +15,18 @@ class grab_weather{
 				preg_match_all('/(?:<th>|<td>|<td nowrap>).*?(?:<\/th>|<\/td>)/',$value,$matches2);
 				$result[date('Y-m-d',$start_time)][$key]=str_replace(array('<td nowrap>','<td>','</td>','<th>','</th>','<br>','&nbsp',';'),'',$matches2[0]); //remove html table tag from data array
 			}
-			$start_time=$start_time+86400;
+			$start_time=$start_time+86400;  //going to the next day
 		}
 		return $result;
 	}
-	public function save_as_csv($path,$data){
+	public function save_as_csv($path,$data){  //given file path and 2D array, saving as csv
 		$fp=fopen($path,'w');
 		foreach($data as $key => $value){
 			fputcsv($fp,$value);
 		}
 		fclose($fp);
 	}
-	public function load_data($path){
+	public function load_data($path){  //given csv file path, returning a 2D array
 		if(!file_exists($path)){
 			//echo 'FILE_NOT_FOUND';
 			return FALSE;
@@ -40,7 +40,7 @@ class grab_weather{
 		}
 		
 	}
-	public function load_specific_hour($path,$hour){
+	public function load_specific_hour($path,$hour){ //given csv file path and hour, returning the specific hour data in 1D array 
 		if(!file_exists($path)){
 			//echo 'FILE_NOT_FOUND';
 			return FALSE;
@@ -56,40 +56,5 @@ class grab_weather{
 	}
 }
 
-//$weather_downloder= new grab_weather();
-//$weather_downloder->station_number='C0X130';
-//$data=$weather_downloder->fetch('20190310','20190311');
-//$weather_downloder->save_as_csv('./data_day/',$data);
-/*
-if(FALSE){
-	$fp=fopen('station.csv','r');
-	$station_list=array();
-	while(($data=fgetcsv($fp))!== FALSE){  //load station list
-		array_push($station_list,$data[0]);
-	}
-	unset($station_list[0]);  //unset 站號
-
-
-	$weather_downloder= new grab_weather();
-	foreach ($station_list as $value){
-		$data=$weather_downloder->fetch($value,'20190320','20190321');
-		if(!file_exists('./data_day/'.$value.'/'))
-		{
-			mkdir('./data_day/'.$value.'/');
-		}
-		//print_r($data);
-		foreach($data as $key1 => $value1){ //loop every day $value=station_number $key1=date $value1=data
-			$weather_downloder->save_as_csv('./data_day/'.$value.'/'.$key1.'.csv',$value1);
-		}
-	}
-}
-*/
-if(FALSE){
-	$weather_class= new grab_weather();
-	print_r($weather_class->load_data('./data_day/466880/2019-03-17.csv'));
-	$weather_class->load_specific_hour('./data_day/C0G790/2019-03-21.csv',10); 
-}
-
 
 ?>
-
